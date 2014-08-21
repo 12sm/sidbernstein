7b26229e6f608a2b68dfac0ee43405f91ecc1ee9
benhardt <ben@ben-hardt.com>
2014-08-21 08:58:59 -0500

    header

--- base.php
+++ base.php
@@ -8,12 +8,12 @@
   <![endif]-->
 
   <?php
-    do_action('get_header');
+    //do_action('get_header');
     // Use Bootstrap's navbar if enabled in config.php
     /*if (current_theme_supports('bootstrap-top-navbar')) {
       get_template_part('templates/header-top-navbar');
     } else {*/
-      get_template_part('templates/header');
+      //get_template_part('templates/header');
     //}
 ?>
 
