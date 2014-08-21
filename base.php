13a1a99d0c60979605ed633545c92b855c333133
benhardt <ben@ben-hardt.com>
2014-08-20 21:19:29 -0500

    remove footer temp

--- base.php
+++ base.php
@@ -37,7 +37,7 @@
     </div><!-- /.content -->
   </div><!-- /.wrap -->
 
-  <?php get_template_part('templates/footer'); ?>
+  <!--<?php get_template_part('templates/footer'); ?>-->
 
 </body>
 </html>
