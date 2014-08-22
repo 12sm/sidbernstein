d550f2116aae531f41566af6d2d5475338702e12
benhardt <ben@ben-hardt.com>
2014-08-22 10:39:10 -0500

    test

--- base.php
+++ base.php
@@ -6,7 +6,7 @@
       <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'roots'); ?>
     </div>
   <![endif]-->
-
+<!--delete me-->
   <?php
     do_action('get_header');
     // Use Bootstrap's navbar if enabled in config.php
