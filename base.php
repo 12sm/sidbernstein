13491c57a9c15c7576fc52cb4c41b1f860946c0a
benhardt <ben@ben-hardt.com>
2014-08-22 10:40:22 -0500

    header

--- base.php
+++ base.php
@@ -6,14 +6,13 @@
       <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'roots'); ?>
     </div>
   <![endif]-->
-<!--delete me-->
   <?php
     do_action('get_header');
     // Use Bootstrap's navbar if enabled in config.php
     /*if (current_theme_supports('bootstrap-top-navbar')) {
       get_template_part('templates/header-top-navbar');
     } else {*/
-      get_template_part('templates/header');
+      //get_template_part('templates/header');
     //}
 ?>
 
