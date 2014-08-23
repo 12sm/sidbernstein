769f1f5322f1353132a168aabac91fdce37177eb
benhardt <ben@ben-hardt.com>
2014-08-22 20:09:38 -0500

    more home

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -1,2 +1,14 @@
 <div class="sidback">
-</div>			
+</div>
+<div class="col-lg-5">
+	<div class="row">
+	</div>
+</div>
+<div class="col-lg-7">
+	<div class="row">
+		<div class="quote">
+		</div>
+		</div class="memorybutton">
+		</div>
+	</div>
+<div>
