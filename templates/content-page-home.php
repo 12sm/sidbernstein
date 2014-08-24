03bdf3a49a63171fdb639457617bc4b984d7a38e
benhardt <ben@ben-hardt.com>
2014-08-24 15:48:53 -0500

    mem butt 2

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -8,8 +8,8 @@
 	<div class="row">
 		<div class="quote">
 		</div>
-		</div class="memory">
-		<a class="btn btn-lg btn-default memorybutton" data-toggle="modal" 
+		<div class="memory">
+		<a class="btn-default memorybutton" data-toggle="modal" 
    		   data-target="#largeModal" 
    		   data-remote="memory-modal.html">Click To Share a Memory</a>
 		</div>
