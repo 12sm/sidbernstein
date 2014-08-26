c42ca94b2fe77ba18638af631a2c82cb11fe53de
benhardt <ben@ben-hardt.com>
2014-08-26 07:57:17 -0500

    build on home

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -1,21 +1,25 @@
 <div class="sidback">
+	<div class="col-md-6 col-sm-6">
+		<div class="row">
+		</div>
+	</div>
+	<div class="col-lg-6 col-sm-6">
+		<div class="row">
+			<div class="quote">
+			</div>
+			<div class="memory">
+			<a class="btn-default memorybutton" data-toggle="modal" 
+	   		   data-target="#largeModal" 
+	   		   data-remote="memory-modal.html">Click To Share a Memory</a>
+			</div>
+		</div>
+	</div>	
+	<div class="col-sm-12">
+		<div class="row">	
+			<?php
+	 			 echo do_shortcode("[wpv-view name='Home Bio']");
+			?>
+		</div>
+	</div>
+</div>
 
-<div class="col-md-6 col-sm-6">
-	<div class="row">
-	</div>
-</div>
-<div class="col-lg-6 col-sm-6">
-	<div class="row">
-		<div class="quote">
-		</div>
-		<div class="memory">
-		<a class="btn-default memorybutton" data-toggle="modal" 
-   		   data-target="#largeModal" 
-   		   data-remote="memory-modal.html">Click To Share a Memory</a>
-		</div>
-	</div>
-<?php
-  echo do_shortcode("[wpv-view name='Home Bio']");
-?>
-<div>
-</div>
