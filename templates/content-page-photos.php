b72cebf46097748b668eea8869a1778abfad2435
benhardt <ben@ben-hardt.com>
2014-08-29 14:45:18 -0500

    page title

--- templates/content-page-photos.php
+++ templates/content-page-photos.php
@@ -1,3 +1,10 @@
+<div class="col-sm-12">
+	<div class="row">
+		<div class="pagetitle">
+			<h1>Photos</h1>
+		</div>
+	</div>
+</div>			
 <?php
   echo do_shortcode("[wpv-view name='photos']");
 ?>
