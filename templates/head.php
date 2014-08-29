e525badd5742c79bfb534aecb63934396db16fac
benhardt <ben@ben-hardt.com>
2014-08-29 11:55:46 -0500

    typekit font in heaad

--- templates/head.php
+++ templates/head.php
@@ -31,4 +31,6 @@ http://12southmusic.com/
 
   <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo home_url(); ?>/feed/">
   <link rel="shortcut icon" href="<?php echo of_get_option('favicon_upload'); ?>" />
+  <script type="text/javascript" src="//use.typekit.net/opi0epy.js"></script>
+  <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
 </head>
