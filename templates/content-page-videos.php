78d0a1afd22e55a16d9ace9747a941ae62770c6b
benhardt <ben@ben-hardt.com>
2014-08-29 15:18:20 -0500

    videos

--- templates/content-page-videos.php
+++ templates/content-page-videos.php
@@ -1,3 +1,14 @@
-<?php
-  echo do_shortcode("[wpv-view name='videos']");
-?>
+<div class="col-sm-12">
+	<div class="row">
+		<div class="pagetitle">
+			<h1>Videos</h1>
+		</div>
+	</div>
+</div>
+<div class="col-sm-12">
+	<div class="row">		
+	<?php<?php
+  		echo do_shortcode("[wpv-view name='videos']");
+	?>
+	</div>
+</div>
