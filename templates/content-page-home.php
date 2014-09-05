ef8e7684689421ce9fd5b5f1abc6c7fee0b86aa5
benhardt <ben@ben-hardt.com>
2014-09-05 12:11:32 -0500

    homepage modal

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -9,9 +9,7 @@
 			<div class="quote">
 			</div>
 			<div class="memory">
-			<a class="memorybutton" data-toggle="modal" 
-	   		   data-target="#largeModal" 
-	   		   data-remote="memory-modal.html">Click To Share a Memory</a>
+	   		 <a type="button" class="memorybutton modal-link" data-toggle="modal" data-target="#memory-form">Click To Share A Memory</a>
 			</div>
 		</div>
 	</div>
@@ -29,6 +27,20 @@
 	 		 echo do_shortcode("[wpv-view name='home-music-player']");
 		?>
 </div>
+</div>
+<div class="modal fade" id="memory-form">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-body">
+                  [cred_form form="Share a story about Sid"]
+                </div>
+                <div class="row modal-footer">
+                  <div class="col-sm-12">
+                    <button type="button" class="text-right btn btn-warning" class="close" data-dismiss="modal"><span aria-hidden="true">Submit Your Story</span></button>
+                  </div>
+                </div>
+              </div>
+            </div>
 </div>
 </div>		
 
