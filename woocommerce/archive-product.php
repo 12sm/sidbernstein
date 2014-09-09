aa0ad1eb89b2a758024d82302852636eb172a7fc
benhardt <ben@ben-hardt.com>
2014-09-08 19:04:49 -0500

    syntax1

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -1,3 +1,3 @@
 <?php
-	echo do_shortcode("[wpv-view name='product category view' wpvproductcat="poster"]")
+	echo do_shortcode("[wpv-view name='product category view' wpvproductcat='poster']")
 ?>	
