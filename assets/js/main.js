0b0d8364171df2820f587572ec5e0c20f06e9b9c
Spencer Forrest Anastasio <anastasio.spencer@gmail.com>
2014-09-10 15:28:42 -0500

    gradient 4.0

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,7 +21,7 @@
   var container;
 
   function gridIt(){
-    $( "body" ).addClass( "gradient" );
+    $( ".backstretch" ).addClass( "gradient" );
     container = document.querySelector('.jsmasonry');
     $(container).masonry();
   }
