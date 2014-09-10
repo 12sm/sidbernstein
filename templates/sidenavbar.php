804cf1de3db824c5ebef02aff4f211d56b297873
Spencer Forrest Anastasio <anastasio.spencer@gmail.com>
2014-09-10 17:30:50 -0500

    social classes... see what I did there?

--- templates/sidenavbar.php
+++ templates/sidenavbar.php
@@ -15,10 +15,10 @@
     </div>
     <?php if (has_nav_menu( 'side_navigation')) : wp_nav_menu(array( 'theme_location'=>'side_navigation', 'menu_class' => 'nav nav-pills nav-stacked')); endif; ?>
     <div class="row">
-      <div class="col-xs-6 social">
+      <div class="col-xs-6 social face">
         <i class="fa fa-facebook-square"></i>
       </div>
-      <div class="col-xs-6 social">
+      <div class="col-xs-6 social tube">
         <i class="fa fa-youtube"></i>
       </div>
     </div>
