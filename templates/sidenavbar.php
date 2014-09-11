d3a8ed2055311fba914d8a66867fbcda20036472
Spencer Forrest Anastasio <anastasio.spencer@gmail.com>
2014-09-10 19:45:33 -0500

    corrects affix 2.0

--- templates/sidenavbar.php
+++ templates/sidenavbar.php
@@ -1,4 +1,4 @@
-<div class="sidenav navbar navbar-inverse" data-spy="affix" data-offset-top="" role="sidenav">
+<div class="sidenav navbar navbar-inverse" data-offset-top="" role="sidenav">
   <div class="navbar-header">
     <button type="button" class="navbar-toggle navbar" data-toggle="collapse" data-target=".navbar-collapse">
       <span class="sr-only">Toggle navigation</span>
@@ -7,7 +7,7 @@
       <span class="icon-bar"></span>
     </button>
   </div>
-  <div class="collapse navbar-collapse" role="navigation">
+  <div class="collapse navbar-collapse affix" role="navigation">
     <div class="navlogo">
       <a href="<?php echo home_url(); ?>/">
         <img src="/wp-content/themes/sidbernstein/assets/img/logo.jpg">
