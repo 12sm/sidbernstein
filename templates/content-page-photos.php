1c64f5e181fc221add264c93db73a62528884db0
Sarah <Sarah@12southmusic.com>
2014-09-11 14:49:59 -0500

    cols and rows and page titles

--- templates/content-page-photos.php
+++ templates/content-page-photos.php
@@ -1,12 +1,12 @@
-<div class="col-sm-12">
-	<div class="row">
+<div class="row">
+	<div class="col-sm-12">
 		<div class="pagetitle">
 			<h1>Photos</h1>
 		</div>
 	</div>
 </div>
-<div class="col-sm-12">
-	<div class="row">		
+<div class="row">
+	<div class="col-sm-12">		
 	<?php
 	  echo do_shortcode("[wpv-view name='photos']");
 	?>
--- templates/content-page-store.php
+++ templates/content-page-store.php
@@ -1,7 +1,14 @@
 <div class="row">
-<div class="col-sm-12">	
-	<?php
-	  echo do_shortcode("[wpv-view name='woo-mainpage']");
-	?>
+	<div class="col-sm-12">
+		<div class="pagetitle">
+			<h1>Photos</h1>
+		</div>
+	</div>
 </div>
+<div class="row">
+	<div class="col-sm-12">	
+		<?php
+		  echo do_shortcode("[wpv-view name='woo-mainpage']");
+		?>
+	</div>
 </div>
