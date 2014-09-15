223ba5e732c69df572c55a4fef8310443c4ec724
Spencer Forrest Anastasio <anastasio.spencer@gmail.com>
2014-09-15 15:16:47 -0500

    sidback

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -1,27 +1,4 @@
 <div class="row sidback">
-  <div class="memleft col-sm-5">
-    <img class="sidportrait" src="../wp-content/themes/sidbernstein/assets/img/sid-portrait.png">
-  </div>
-  <div class="memright col-sm-7">
-    <h1>
-
-
-
-
-      <div class="col-xs-12 quote">
-        <img src="../wp-content/themes/sidbernstein/assets/img/home-quote.png">
-      </div>
-    </div>
-    <div class="row membutt">
-      <div class="col-xs-12">
-        <button type="button" class="memorybutton modal-link" data-toggle="modal" data-target="#memory-form">Click To Share A Memory</button>
-      </div>
-  </div>
-</div>
-
-
-
-<div class="row sidback">
   <div class="memright col-xs-6 col-xs-offset-5">
     <div class="row">
       <div class="col-xs-12 quote">
