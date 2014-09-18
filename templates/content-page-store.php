3b571f5e7d1523cb55ceb927558e5bc7077fa701
Sarah <Sarah@12southmusic.com>
2014-09-18 16:21:17 -0500

    col for vault

--- templates/content-page-store.php
+++ templates/content-page-store.php
@@ -1,14 +1,16 @@
-<div class="row">
-	<div class="col-sm-12">
-		<div class="pagetitle">
-			<h1>Store: Sid's Vault</h1>
+<div class="col">
+	<div class="row">
+		<div class="col-sm-12">
+			<div class="pagetitle">
+				<h1>Store: Sid's Vault</h1>
+			</div>
 		</div>
 	</div>
-</div>
-<div class="row">
-	<div class="col-sm-12">	
-		<?php
-		  echo do_shortcode("[wpv-view name='woo-mainpage']");
-		?>
+	<div class="row">
+		<div class="col-sm-12">	
+			<?php
+			  echo do_shortcode("[wpv-view name='woo-mainpage']");
+			?>
+		</div>
 	</div>
 </div>
