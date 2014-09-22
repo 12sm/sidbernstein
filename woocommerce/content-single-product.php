d7ed4065a96cb02d4b5196c5106f2b64834976e7
Spencer Forrest Anastasio <anastasio.spencer@gmail.com>
2014-09-22 14:14:07 -0500

    single product view edits

--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -4,8 +4,8 @@
  *
  * Override this template by copying it to yourtheme/woocommerce/content-single-product.php
  *
- * @author 		WooThemes
- * @package 	WooCommerce/Templates
+ * @author     WooThemes
+ * @package   WooCommerce/Templates
  * @version     1.6.4
  */
 
@@ -13,29 +13,29 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 ?>
 
 <?php
-	/**
-	 * woocommerce_before_single_product hook
-	 *
-	 * @hooked wc_print_notices - 10
-	 */
-	 do_action( 'woocommerce_before_single_product' );
+  /**
+   * woocommerce_before_single_product hook
+   *
+   * @hooked wc_print_notices - 10
+   */
+   do_action( 'woocommerce_before_single_product' );
 
-	 if ( post_password_required() ) {
-	 	echo get_the_password_form();
-	 	return;
-	 }
+   if ( post_password_required() ) {
+     echo get_the_password_form();
+     return;
+   }
 ?>
 
 <div itemscope itemtype="<?php echo woocommerce_get_product_schema(); ?>" id="product-<?php the_ID(); ?>" <?php post_class(); ?>>
-	
-	<?php 
-	$theshortcode = "[wpv-view name='single-product' theid='".the_ID()."']";
-	echo do_shortcode($theshortcode);
-	
-	 ?>
+
+  <?php
+  $theshortcode = "[wpv-view name='single-product' theid='".the_ID()."']";
+  echo do_shortcode($theshortcode);
+
+   ?>
 
 
-	<meta itemprop="url" content="<?php the_permalink(); ?>" />
+  <meta itemprop="url" content="<?php the_permalink(); ?>" />
 
 </div><!-- #product-<?php the_ID(); ?> -->
 
