b464893e3febb5a9a16a20a63abfa29e15089583
Spencer Forrest Anastasio <anastasio.spencer@gmail.com>
2014-09-25 13:30:52 -0500

    home

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -19,25 +19,16 @@
   <?php the_content(); ?>
   <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
 <?php endwhile; ?>
-
-
-
 <div class="row sidhomebio">
   <div class="homebio col-xs-12">
     <?php echo do_shortcode( "[wpv-view name='home-bio']"); ?>
   </div>
 </div>
-
-
-
 <div class="row sidhomestore">
   <div class="col-xs-10 col-xs-offset-1 homestore">
     <?php echo do_shortcode( "[wpv-view name='home-store']"); ?>
   </div>
 </div>
-
-
-
 <div class="row sidhomealbum">
   <div class="col-xs-10 col-xs-offset-1 homealbum">
     <?php echo do_shortcode( "[wpv-view name='home-music-player']"); ?>
