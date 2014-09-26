e2f0e807a0c9e8353e60f2851e8e438c7b40f10b
Spencer Forrest Anastasio <anastasio.spencer@gmail.com>
2014-09-26 11:45:36 -0500

    jfhks

--- templates/content-page-articles.php
+++ templates/content-page-articles.php
@@ -1,10 +1,10 @@
 <div class="row">
-  <div class="col-sm-12">
+  <div class="col-sm-10 col-sm-offset-1">
     <div class="page-header">
-  <h1>
-    <?php echo roots_title(); ?>
-  </h1>
-</div>
+      <h1>
+        <?php echo roots_title(); ?>
+      </h1>
+    </div>
   </div>
 </div>
 <div class="row">
--- templates/content-page-videos.php
+++ templates/content-page-videos.php
@@ -1,16 +1,16 @@
 <div class="row">
-	<div class="col-sm-12">
-		<div class="page-header">
-	<h1>
-		<?php echo roots_title(); ?>
-	</h1>
-</div>
-	</div>
+  <div class="col-sm-10 col-sm-offset-1">
+    <div class="page-header">
+      <h1>
+        <?php echo roots_title(); ?>
+      </h1>
+    </div>
+  </div>
 </div>
 <div class="row">
-	<div class="col-sm-12">
-	<?php
-		echo do_shortcode("[wpv-view name='videos']");
-	?>
-	</div>
+  <div class="col-sm-12">
+    <?php
+      echo do_shortcode("[wpv-view name='videos']");
+    ?>
+  </div>
 </div>
